209f78cbbad67473bc40307d93ef9f71c927d46d
Moroz812 <32704620+Moroz812@users.noreply.github.com>
2025-08-31 18:13:22 +0300

    создание класса FileWrittterUtil, реализация методов агрегации в файл, метод записи логических операций в файл и тд.
    * создание класса FileWrittterUtil, реализаця метода агрегации в файл, создание экземпляра класса в Main, разделение и заполнение в цикле массива на 2 части
    
    * +метод записи результатов лог операций в файл

---
+++ src/FileWriterUtil.java
@@ -0,0 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+// класс для записи данных в файл
+public class FileWriterUtil {
+
+    // метод для записи результатов агрегации в файл
+    public void writeAggregationToFile(Map<Integer, Integer> aggregation, String filename) throws IOException {
+        FileWriter writer = new FileWriter(filename); // вызываем конструктор FileWriter
+
+        // проходим по всем записям в карте агрегации
+        for (Map.Entry<Integer, Integer> entry : aggregation.entrySet()) {
+            // записываем строку в формате "число - количество"
+            writer.write(entry.getKey() + " - " + entry.getValue() + ";\n");
+        }
+        writer.close(); // закрываем файл
+    }
+
+    // метод для записи результатов логических операций в файл
+    public void writeLogicalOperationsToFile(String operation, String result, String filename) throws IOException {
+        FileWriter writer = new FileWriter(filename, true); // cоздаём FileWriter с возможнотью дозаписи в файл
+        writer.write(result + "\n"); // запись результата операции
+        writer.close(); // закрытие файла
+    }
+}
